6e5c6e7351593b63f52603b5b1426695028ff216
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-09-21 20:34:17 -0400

    Express module completed

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,12 +9,13 @@ const adminRoutes = require('./routes-week2/admin.js');
 const shopRoutes = require('./routes-week2/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public-week2')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views-week2', '404.html'));
 });
 
 app.listen(3000);
