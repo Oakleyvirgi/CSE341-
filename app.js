fb56d64183e5a075fa75e381dc497bf7acb7ec3b
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-09-26 00:03:10 -0400

    week 2

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-const adminRoutes = require('./routes-week2/admin.js');
-const shopRoutes = require('./routes-week2/shop.js');
+/*app.engine('hbs', expressHbs({
+    layoutsDir: 'views-week2/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+}));*/
+app.set('views engine', 'ejs');
+app.set('views-week2', 'views-week2');
+
+const adminData = require('./routes-week2/admin');
+const shopRoutes = require('./routes-week2/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public-week2')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views-week2', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
--- routes/ta02.js
+++ routes/ta02.js
@@ -1,59 +1,61 @@
+//TA02 PLACEHOLDER
+// Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+
+const users = ["User1", "User2", "User3"];
+
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    users: users
   });
 });
-module.exports = router;
 
-/*TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself! 
-const express = require('express');
-const router = express.Router();
-​
-const usernames = [];
-​
-router.get('/',(req, res, next) => {
-    renderTa02(res);
+router.post("/addUser", (req, res, next) => {
+ let addError = false;
+
+  if (users.includes(req.body.addUser)) {
+    addError = true;
+  }else {
+    users.push(req.body.addUser);
+    
+  }
+  
+  
+  
+  res.render('pages/ta02', {
+    title: 'Team Activity 02',
+    path: '/ta02', // For pug, EJS
+    //activeTA03: true, // For HBS
+    //contentCSS: true, // For HBS
+    users: users,
+    addError: addError,
+    removeError: false
+  });
 });
-​
-router.post('/addUser', (req, res, next) => {
-    const index = usernames.indexOf(req.body.username);
-    let message = '';
-    if (index > -1) {
-        message = "Duplicate User!"
-    }
-    else {
-        usernames.push(req.body.username);
-    }
-    renderTa02(res, message);
+
+router.post("/removeUser", (req, res, next) => {
+  const index = users.indexOf(req.body.removeUser);
+  let removeError = false;
+  if (index > -1) {
+      users.splice(index, 1);
+  }else{
+    removeError = true;
+  }
+  res.render('pages/ta02', {
+    title: 'Team Activity 02',
+    path: '/ta02', // For pug, EJS
+    //activeTA03: true, // For HBS
+    //contentCSS: true, // For HBS
+    users: users,
+    addError: false,
+    removeError: removeError
+  });
 });
-​
-router.post('/removeUser', (req, res, next) => {
-    const index = usernames.indexOf(req.body.username);
-    let message = '';
-    if (index > -1) {
-        usernames.splice(index, 1);
-    }
-    else {
-        message = "User does not exist!";
-    }
-    renderTa02(res, message);
-});
-​
-function renderTa02(res, message = '') {
-    res.render('pages/ta02', { 
-        title: 'Team Activity 02',
-        names: usernames, 
-        errorMessage: message,
-        path: '/ta02', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
-    });
-}
-​
-module.exports = router;*/
+
+module.exports = router;
