2a298244c4ed01c0e942855468c53eaca58d374c
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-09-27 23:42:18 -0400

    assigment finished- works fine

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const app = express();
 
@@ -17,14 +17,14 @@ app.set('views-week2', 'views-week2');
 const adminData = require('./routes-week2/admin');
 const shopRoutes = require('./routes-week2/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public-week2')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
