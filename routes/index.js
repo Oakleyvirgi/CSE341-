c678489485ff97f2f6251b13a490cf887bdea8a2
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-10-08 20:54:18 -0400

    organization partI?

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const routes = require('express').Router();
+const classActivities = require('./classRoutes');
+const teamActivities = require('./teamRoutes');
+const proveActivities = require('./proveRoutes');
+
+routes  
+    .use('./classActivities', classActivities)
+    .use('./teamActivities', teamActivities)
+    .use('./proveAssignments', proveActivities)
+
+    .get('./', (req, res, next) => {
+        res.render('pages/index', {title: 'Welcome to my CSE341 repo', path:'/'});
+    })
+    .use((req, res, next) => {
+        res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
+    })
+
+module.exports = routes;
