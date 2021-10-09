d3d1ea35989d5a8202942c05e44e9c9811ce906c
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-10-09 16:38:23 -0400

    test

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ routes
     .use('./proveAssignments', proveActivities)
 
     .get('/', (req, res, next) => {
-        res.render('pages/index', {title: 'Welcome to my CSE341 repo', path:'/'});
+        res.render('pages/index', {title: 'Test Welcome to my CSE341 repo', path:'/'});
     })
     .use((req, res, next) => {
         res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
