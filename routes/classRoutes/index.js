3427ba2b2eb190c1c3b68b34c4f60f7fe1be9000
Oakleyvirgi <77085743+Oakleyvirgi@users.noreply.github.com>
2021-10-08 22:00:24 -0400

    organization partII

---
+++ routes/classRoutes/index.js
@@ -0,0 +1,10 @@
+const classRoutes = require('express').Router();
+
+classRoutes.use('/02', require('./w02'))
+    .get('/', (req, res, next) => {
+        res.render('pages/classActivities/', {
+            pageTitle: 'Class Activities',
+            path: '/classActivities'});
+    });
+
+module.exports = classRoutes;
